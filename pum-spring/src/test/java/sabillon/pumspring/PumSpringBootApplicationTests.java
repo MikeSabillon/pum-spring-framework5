cddaa853fda17f1043a1a41304340e84575ab3e0
Miguel Sabillon <miguel.sabillonmartinez@plexus.es>
2019-01-08 16:36:24 +0100

    Initial Commit

---
+++ pum-spring/src/test/java/sabillon/pumspring/PumSpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package sabillon.pumspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PumSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
